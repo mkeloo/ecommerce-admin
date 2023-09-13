6ba8abd05e3787939971cfcf80740e3484d2d473
mkeloo <mkeloo@ufl.edu>
2023-09-13 06:19:32 -0400

    billboard entity done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,15 +6,23 @@ import { useRouter, useParams } from 'next/navigation';
 import { Heading } from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
+import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
 
-export const BillboardClient = () => {
+import { BillboardColumn, columns } from './columns';
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -25,6 +33,10 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -7,7 +7,7 @@ export const Heading: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div className="flex flex-col space-y-2">
       <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-      <p className="text-sm text-muted foreground">{description}</p>
+      <p className="text-sm text-gray-600 foreground">{description}</p>
     </div>
   );
 };
